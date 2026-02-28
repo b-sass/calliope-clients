cd6a843424747c1ae7324d089d9dd201ed33433a
Bartosz Sass <sassbartek@gmail.com>
2026-02-28 01:09:24 +0000

    Added a component for the Home page.

--- apps/desktop/src/Home/Home.tsx
+++ apps/desktop/src/Home/Home.tsx
@@ -1,7 +1,12 @@
 import { BookCard } from "../ui/BookCard";
+import styles from "./home.module.css";
 
 const Home = () => {
-  return <BookCard title={"title"} />;
+  return (
+    <div className={styles.home}>
+      {/* <BookCard title={"A very long title"} progress={"67%"} /> */}
+    </div>
+  );
 };
 
 export { Home };
