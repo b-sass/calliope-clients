df6a9960852b651d8274b786aee0e4eac364050a
Bartosz Sass <sassbartek@gmail.com>
2026-03-04 03:14:22 +0000

    Added navbar icons from google fonts.

---
+++ apps/desktop/src/assets/icons/Icons.ts
@@ -0,0 +1,15 @@
+import book from "./book.svg";
+import bookmarks from "./bookmarks.svg";
+import home from "./home.svg";
+import lan from "./lan.svg";
+import menu from "./menu.svg";
+import settings from "./settings.svg";
+
+export const Icons = {
+  book,
+  bookmarks,
+  home,
+  lan,
+  menu,
+  settings,
+} as const;
