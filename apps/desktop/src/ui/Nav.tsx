8e0cef522613c51408bd6afb877c960147339611
Bartosz Sass <sassbartek@gmail.com>
2026-03-04 03:16:10 +0000

    Basic responsive navbar.

--- apps/desktop/src/ui/Nav.tsx
+++ apps/desktop/src/ui/Nav.tsx
@@ -1,14 +1,66 @@
-const Nav = () => {
+import styles from "./nav.module.css";
+import { Icons } from "../assets/icons/Icons";
+import { useState } from "react";
+
+const elements = [
+  [Icons.home, "Home"],
+  [Icons.book, "Shelf"],
+  [Icons.bookmarks, "Bookmarks"],
+  [Icons.lan, "Online Library"],
+  [Icons.settings, "Settings"],
+];
+
+const NavElement = ({
+  icon,
+  label,
+  onClick,
+}: {
+  icon: any;
+  label?: string;
+  onClick?: () => void;
+}) => {
   return (
-    <div>
-      <li>
-        <ul>Open</ul>
-        <ul>Option 1</ul>
-        <ul>Option 2</ul>
-        <ul>Option 3</ul>
-        <ul>Option 4</ul>
-        <ul>Option 5</ul>
-      </li>
+    <div className={styles["nav-element"]} onClick={onClick}>
+      <img src={icon} />
+      {/* Don't render label element if not present */}
+      {label ? <p style={{ display: "none" }}>{label}</p> : null}
+    </div>
+  );
+};
+
+const Nav = () => {
+  const [navState, setNavState] = useState(false);
+
+  const toggleNav = (navState: boolean) => {
+    setNavState(!navState);
+    console.log(navState);
+  };
+
+  return (
+    <div className={styles.nav}>
+      <ul className={!navState ? styles["long-nav"] : ""}>
+        {/* Hamburger menu icon */}
+        <li>
+          <NavElement
+            icon={Icons.menu}
+            onClick={() => {
+              toggleNav(navState);
+            }}
+          />
+        </li>
+        {/* Rest of icons */}
+        {elements.map((e) => {
+          console.log(styles.last);
+          return (
+            <li
+              key={e[1]}
+              className={`${styles.link} ${e[1] === "Settings" ? styles.last : ""}`}
+            >
+              <NavElement icon={e[0]} label={e[1]} />
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
